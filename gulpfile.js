2b2c10b38c466a823c7185a66d87f5df4b43973d
Artem Drobotenko <artem_drobotenko@epam.com>
2015-07-23 18:41:15 +0300

    gulp minify task added

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('minify', function(){
+    gulp.src('src/scripts/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./build'));
 });
