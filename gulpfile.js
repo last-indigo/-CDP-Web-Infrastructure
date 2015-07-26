b9e2ee1b206f5321b9e5a9ed45ece9c65b46893c
last-indigo <artem.drobotenko@gmail.com>
2015-07-26 12:11:12 +0300

    added jscs task and config file

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var jscs = require('gulp-jscs');
 
+var paths = {
+  scripts: 'src/scripts/**/*.js'
+}
 
 gulp.task('default', function() {
   // place code for your default task here
 });
+ 
+gulp.task('jscs', function () {
+    return gulp.src( paths.scripts )
+        .pipe(jscs());
+});
 
 gulp.task('minify', function(){
-    gulp.src('src/scripts/**/*.js')
+    return gulp.src( paths.scripts )
         .pipe(uglify())
         .pipe(gulp.dest('./build'));
 });
